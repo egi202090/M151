27e31f801ae814c49272a30505ce0d2b6cb69776
egi202090 <egzon_1997@outlook.com>
2018-04-12 16:06:11 +0200

    egzu

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 
-   <link rel="stylesheet" href="/M151/assets/css/styles.css">
+    <link rel="stylesheet" href="/M151/assets/css/styles.css">
     <link rel="stylesheet" href="/M151/assets/css/login.css">
+    <link rel="stylesheet" href="/M151/assets/css/edit.css">
     <script type="text/javascript" src="/M151/assets/js/nav.js"></script>
 </head>
 <body>
@@ -14,7 +15,8 @@
     <a href="#about">Upload</a>
     <div class="topnav-right">
         <a href="/M151/user/login">Login</a>
-        <a href="">Register</a>
+        <a href="/M151/user/register">Register</a>
+        <a href="/M151/user/editProfile">Profil</a>
         <a href="">Logout</a>
     </div>
     <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
